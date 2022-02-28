07d88ce5d31e858ed7e3221fdf9221ae70a49e11
xCthulhuFtagn <mkolodyazhny@gmail.com>
2022-02-28 17:42:33 +0300

    Fixed and explained in comments the logic of copying. [no tests]

--- main.cpp
+++ main.cpp
@@ -49,35 +49,48 @@ int file_copy(char * source, char * dest) {
 }
 
 //recursive for first realisation
-void dir_copy(char* from_ch, char* to){
+void dir_copy(char* from_ch, char* to_ch){
     struct stat st;
     dirent *obj;
     DIR *origin, *subdir;
     char buf[20];
-    if((origin = opendir(from_ch)) == NULL){
+    if((origin = opendir(from_ch)) == NULL || chdir(to_ch) < 0){
         char buf[PATH_MAX + 26] = "Could not open directory ";
         strcat(buf, from_ch);
         perror(buf);
         return;
     }
+    /*
+        логика: 
+        гуляем при помощи chdir по архивным файлам, чтобы можно было без пота создавать новые объекты
+        гуляем при помощи opendir и ПОЛНЫХ адресов по архивируемым директориям
+    */
     while ((obj = readdir(origin)) != NULL){
-        //file_copy(./file, ./arch/file)
         lstat(obj->d_name, &st);
         if(S_ISDIR(st.st_mode)){
             if(strcmp(obj->d_name, ".") || strcmp(obj->d_name, "..")) {
                 //need 2 copy them? yes!
             } else{
-                // enter directory
-                char to_subdir[PATH_MAX];
-                strcpy(to_subdir, to);
-                strcat(to_subdir, obj->d_name);
-                dir_copy(obj->d_name, to_subdir);                
+                // found directory
+                if(mkdir(obj->d_name, S_IRWXO || S_IRWXG || S_IRWXU) < 0){
+                    char buf[PATH_MAX + 19] = "Could not copy ";
+                    strcat(buf, obj->d_name);
+                    strcat(buf, " dir");
+                    perror(buf);
+                }
+                else{
+                    char from_subdir[PATH_MAX];
+                    strcpy(from_subdir, from_ch);
+                    strcat(from_subdir, "/");
+                    strcat(from_subdir, obj->d_name);
+                    dir_copy(from_subdir, obj->d_name);   
+                }             
             }
         } else{
             //copy file
-            if (file_copy(strcat("./", obj->d_name), strcat(strcat(to, "/"), obj->d_name)) == 0){
+            if (file_copy(strcat(strcat(from_ch, "/"), obj->d_name), obj->d_name) == 0){
                 //if the copy is successfull -> print into log
-                write(log_desc, "\"", 1);
+                write(log_desc, "File \"", 1);
                 write(log_desc, obj->d_name, strlen(obj->d_name));
                 write(log_desc, "\", size: ", 10);
                 fstat(log_desc, &st);
@@ -86,8 +99,10 @@ void dir_copy(char* from_ch, char* to){
                 write(log_desc, "\n", 2);
             }
             else{
-                printf("File %s could not not be copied", obj->d_name);
-                perror("");
+                char buf[PATH_MAX + 30] = "File ";
+                strcat(buf, obj->d_name);
+                strcat(buf, " could not not be copied");
+                perror(buf);
             }
         }
     }
@@ -157,37 +172,13 @@ int main(int argc, char* argv[]){
         write(log_desc, "\n", 2);
         chdir("..");
         if(argv[3] == "-d"){ // archive whole directory
-            //write(log_desc, "Those files were copied:\n", 28);
-            dir_copy(argv[4], strcat(strcat(path, "/"), argv[2]));
+            strcat(path, "/");
+            strcat(path, argv[4]);
+            //inputting full adress of input
+            dir_copy(path, argv[2]);
         }
         else if(argv[3] == "-f"){
-            /*
-            struct stat st;
-            while ((file = readdir(arch)) != NULL){
-                //file_copy(./file, ./arch/file)
-                bool found = false;
-                for(size_t i = 4; i + 1 < argc; ++i){
-                    if(strcmp(file->d_name, argv[i]) == 0){
-                        found = true;
-                        break;
-                    }
-                }
-                if (found && file_copy(strcat("./", file->d_name), strcat(strcat(strcat("./", argv[2]), "/"), file->d_name)) == 0){
-                    //if file is in the list => if the copy is successfull -> print into log
-                    write(log_desc, "\"", 1);
-                    write(log_desc, file->d_name, strlen(file->d_name));
-                    write(log_desc, "\" size: ", 9);
-                    fstat(log_desc, &st);
-                    sprintf(buf, "%ld", st.st_size);
-                    auto m = st.st_mode;
-                    write(log_desc, buf, strlen(buf));
-                    write(log_desc, "\n", 2);
-                }
-                else{
-                    printf("%s could not not be copied\n", file->d_name);
-                }
-            }
-            */
+            //list of files
         }
         else{
             perror("Wrong parameter for input\n");
