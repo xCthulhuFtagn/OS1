5792b55f4b7903664acf05b6be274277a45702f5
CthulhuFtagn <mkolodyazhny@gmail.com>
2022-03-05 23:36:11 +0300

    New way of archivation (into a file). Need to add -f variant still. [tests required]

--- main.c
+++ main.c
@@ -23,9 +23,9 @@ int archive_desc;
 int log_desc;
 
 int file_pack(char * source, char lvl){
-    write(log_desc, "Packaging file ", 15);
-    write(log_desc, source, strlen(source));
-    write(log_desc, "...\n", 4);
+    // write(log_desc, "Packaging file ", 15);
+    // write(log_desc, source, strlen(source));
+    // write(log_desc, "...\n", 4);
     write(archive_desc, &lvl, sizeof(lvl));
     write(archive_desc, "f", 1);
     write(archive_desc, source, strlen(source));
@@ -69,9 +69,9 @@ int dir_pack(char* from_ch, char lvl){
     struct dirent *obj;
     DIR *origin, *subdir;
     char buf[PATH_MAX + 200];
-    write(log_desc, "Packaging directory ", 21);
-    write(log_desc, from_ch, strlen(from_ch));
-    write(log_desc, "...\n", 4);
+    // write(log_desc, "Packaging directory ", 21);
+    // write(log_desc, from_ch, strlen(from_ch));
+    // write(log_desc, "...\n", 4);
     if ((origin = opendir(from_ch)) == NULL) {
         char buf1[PATH_MAX + 26] = "Could not open source directory ";
         strcat(buf1, from_ch);
@@ -94,7 +94,8 @@ int dir_pack(char* from_ch, char lvl){
             return -1;
         }
         char output[BUFSIZ] = "";
-        strnset(output, '\t', lvl);
+        for (int i = 0; i < lvl; ++i) output[i] =  '\t';
+        output[lvl] = '\0';
         if (S_ISDIR(st.st_mode)) {
             if (strcmp(obj->d_name, ".") != 0 && strcmp(obj->d_name, "..") != 0) {
                 // found directory
@@ -144,14 +145,8 @@ int main(int argc, char* argv[]){
         perror("Not enough arguments");
         return -1;
     }
-    if (strcmp(argv[1], "-create") == 0) {
+    if (strcmp(argv[1], "-pack") == 0) {
         int i;
-        if ((i = mkdir(argv[2], S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH)) < 0) {
-            //Read + Write + Execute: S_IRWXU (User), S_IRWXG (Group), S_IRWXO (Others)
-            perror("Could not create archive");
-            return -1;
-        }
-        if (chdir(argv[2]) < 0) perror("Can't change directory");
         char bruh[PATH_MAX];
         strcpy(bruh, path);
         log_desc = open(strcat(bruh, "/log.txt"), O_RDWR | O_APPEND | O_CREAT, S_IRWXO | S_IRWXG | S_IRWXU);
@@ -168,26 +163,12 @@ int main(int argc, char* argv[]){
         // date + ls
         time_t t = time(NULL);
         struct tm d = *localtime(&t);
-        char buf[20];
-        write(log_desc, "Date and hour : ", 17);
-        sprintf(buf, "%d", d.tm_mday);
+        char buf[400];
+        write(log_desc, "Hour and date : ", 17);
+        //putenv("TZ=Europe/Moscow");
+        //sprintf(buf, asctime(localtime(&t)));
+        strftime(buf, 400, "%X %d %B %Y\n", &d);
         write(log_desc, buf, strlen(buf));
-        write(log_desc, "/", 1);   
-        sprintf(buf, "%d", d.tm_mon);
-        write(log_desc, buf, strlen(buf));
-        write(log_desc, "/", 1);
-        sprintf(buf, "%d", d.tm_year);
-        write(log_desc, buf, strlen(buf));
-        write(log_desc, " ", 1);
-        sprintf(buf, "%d", d.tm_hour);
-        write(log_desc, buf, strlen(buf));
-        write(log_desc, ":", 1);
-        sprintf(buf, "%d", d.tm_min);
-        write(log_desc, buf, strlen(buf));
-        write(log_desc, ":", 1);
-        sprintf(buf, "%d", d.tm_sec);
-        write(log_desc, buf, strlen(buf));
-        write(log_desc, "\n", 2);
         chdir("..");
         if (strcmp(argv[3], "-d") == 0) { // archive whole directory
             strcat(path, "/");
@@ -207,7 +188,7 @@ int main(int argc, char* argv[]){
             return -1;
         }
     }
-    else if (strcmp(argv[1], "-extract") == 0) {
+    else if (strcmp(argv[1], "-unpack") == 0) {
         //
     }
     else {
