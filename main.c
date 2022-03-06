eff47a8a6fe5913f217d2d4036338486fbfcbf3e
CthulhuFtagn <mkolodyazhny@gmail.com>
2022-03-06 19:24:41 +0300

    Universal packing and base for compression added

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <time.h>
+#include <stdint.h>
+
+//wow, bool in c? yeah
+#include <stdbool.h>
 
 #include <fcntl.h>
 #include <sys/types.h>
@@ -11,50 +15,68 @@
 #include <unistd.h>
 
 // for files & dir operations
-// tar {options} archive.tar {sources} [file1 file2 file3 ...] | dir
-// sources: -d = directories / -f = files
-// options:  -pack / -unpack
+// tar {options} archive.tar [src1 src2 src3 ...] | dir
+// options:  -p = pack / -u = unpack
+// if pack, then sources (directories|files)
+// if unpack, then ONLY directory
 
-// struct {
-//     uint64_t freq[256];
-// } BRUH;
+struct node{
+    short let;
+    short left, right;
+};
+
+struct trio{
+    short let;
+    uint64_t freq;
+    short ind;
+};
+
+struct cmap{
+    struct trio freqlet[256];
+    struct node letters[1 << 16];
+} hash_tree;
+
+int cmp_trio(struct trio a, struct trio b){
+    return b.freq - a.freq;
+}
+
+void init_hash_tree(){
+    for (int i = 0; i < 256; ++i) {
+        hash_tree.freqlet[i].let = i;
+        hash_tree.freqlet[i].freq = 0;
+        hash_tree.freqlet[i].ind = i;
+        hash_tree.letters[i].let = i;
+        hash_tree.letters[i].left = -1;
+        hash_tree.letters[i].right = -1;
+    }
+}
+
+void sort_hash_tree(){
+    uint64_t sum = 0;
+    qsort(hash_tree.freqlet, 256, sizeof(struct trio), cmp_trio);
+    for (int i = 0; i < 256; ++i) {
+        printf("%h - %lu\n", hash_tree.freqlet[i].let, hash_tree.freqlet[i].freq);
+        sum += hash_tree.freqlet[i].freq;
+    }
+    if (sum > 256 * 16) {
+        puts("There is reason to archive");
+    }
+    else {
+        puts("There is no reason to archive");
+    }
+    //
+    for (int i = 255, j = 0; i > 0; --i) {
+        if (hash_tree.freqlet[i].freq == 0) continue;
+        hash_tree.letters[j].let = -1;
+        hash_tree.letters[j].left = hash_tree.freqlet[i - 1].ind;
+        hash_tree.letters[j].right = hash_tree.freqlet[i].ind;
+        hash_tree.freqlet[i - 1].ind = j;
+        hash_tree.freqlet[i - 1].freq += hash_tree.freqlet[i].freq;
+    }
+}
 
 int archive_desc;
 int log_desc;
-
-int file_pack(char * source, char lvl){
-    // write(log_desc, "Packaging file ", 15);
-    // write(log_desc, source, strlen(source));
-    // write(log_desc, "...\n", 4);
-    write(archive_desc, &lvl, sizeof(lvl));
-    write(archive_desc, "f", 1);
-    write(archive_desc, source, strlen(source));
-	int src_desc, dst_desc, in, out;
-	if ((src_desc = open(source, O_RDONLY)) == -1) {
-        char buf[PATH_MAX + 29] = "Could not open source file ";
-        strcat(buf, source);
-        strcat(buf, "\n");
-        perror(buf);
-		return -1;
-	}
-    char buf[BUFSIZ];
-    char fail = 0;
-    ssize_t read_status, write_status;
-    while ((read_status = read(src_desc, buf, BUFSIZ)) > 0) {
-        if ((write_status = write(archive_desc, buf, BUFSIZ)) == -1) {
-            fail = 1;
-            break;
-        }
-    }
-    if (read_status == -1) fail = 1;
-	close(src_desc);
-
-	if (fail) {
-        perror("Copying error occured");
-        return -1;
-    }
-    else return 0;
-}
 
 void CutAdress(char* adr){
     char* slash = strrchr(adr, '/');
@@ -63,73 +85,105 @@ void CutAdress(char* adr){
     }
 }
 
-//recursive for first realisation
-int dir_pack(char* from_ch, char lvl){
+void format_output(char* output, char lvl){
+    for (char i = 0; i < lvl; ++i) output[i] =  '\t';
+    output[lvl] = '\0';
+}
+
+int pack(char* from_ch, char lvl){
     struct stat st;
-    struct dirent *obj;
-    DIR *origin, *subdir;
     char buf[PATH_MAX + 200];
-    // write(log_desc, "Packaging directory ", 21);
-    // write(log_desc, from_ch, strlen(from_ch));
-    // write(log_desc, "...\n", 4);
-    if ((origin = opendir(from_ch)) == NULL) {
-        char buf1[PATH_MAX + 26] = "Could not open source directory ";
-        strcat(buf1, from_ch);
-        perror(buf1);
+    if (lstat(from_ch, &st) == -1) {
+        perror("lstat broke");
+        printf("current from_ch = %s", from_ch);
         return -1;
     }
-    write(archive_desc, &lvl, sizeof(lvl));
-    write(archive_desc, "d", 1);
-    write(archive_desc, from_ch, strlen(from_ch));
-    /*
-        логика:
-        гуляем при помощи chdir по архивным файлам, чтобы можно было без пота создавать новые объекты
-        гуляем при помощи opendir и ПОЛНЫХ адресов по архивируемым директориям
-    */
-    strcat(from_ch, "/");
-    while ((obj = readdir(origin)) != NULL) {
-        strcat(from_ch, obj->d_name);
-        if (lstat(from_ch, &st) == -1) {
-            perror("lstat broke");
+    char output[BUFSIZ] = "";
+    bool failure = false;
+    if (S_ISDIR(st.st_mode)) {
+        struct dirent *obj;
+        DIR *origin, *subdir;
+        format_output(output, lvl);
+        strcat(output, "Dir ");
+        strcat(output, strrchr(from_ch, '/') + 1);
+        strcat(output, "{\n");
+        strcat(from_ch, "/");
+        write(log_desc, output, strlen(output));
+        if ((origin = opendir(from_ch)) == NULL) {
+            char buf1[PATH_MAX + 26] = "Could not open source directory ";
+            strcat(buf1, from_ch);
+            perror(buf1);
+            format_output(output, lvl);
+            strcpy(output, "} FAILURE;");
+            write(log_desc, output, strlen(output));
             return -1;
         }
-        char output[BUFSIZ] = "";
-        for (int i = 0; i < lvl; ++i) output[i] =  '\t';
-        output[lvl] = '\0';
-        if (S_ISDIR(st.st_mode)) {
+        write(archive_desc, &lvl, sizeof(lvl));
+        write(archive_desc, "d", 1);
+        write(archive_desc, from_ch, strlen(from_ch));
+        while ((obj = readdir(origin)) != NULL) {
+            strcat(from_ch, obj->d_name);
             if (strcmp(obj->d_name, ".") != 0 && strcmp(obj->d_name, "..") != 0) {
                 // found directory
-                strcat(output, "Dir ");
-                strcat(output, obj->d_name);
-                strcat(output, ": ");
-                if (dir_pack(from_ch, lvl + 1) == 0) {
-                    strcat(output, "SUCCESS\n");
-                } else {
-                    strcat(output, "FAIL\n");
-                }
+                if(pack(from_ch, lvl + 1) != 0) failure = true;
             }
+            CutAdress(from_ch);
+        }
+        from_ch[strlen(from_ch)-1] = '\0';
+        CutAdress(from_ch);
+        closedir(origin);
+        format_output(output, lvl);
+        if(failure) strcat(output, "} FAIL\n");
+        else strcat(output, "} SUCCESS\n");
+        write(log_desc, output, strlen(output));
+    }
+    else { //file found
+        bool lstat_broken = false;
+        format_output(output, lvl);
+        strcat(output, "File ");
+        strcat(output, strrchr(from_ch, '/') + 1);
+        strcat(output, " (");
+        //mb %d
+        if (lstat(from_ch, &st) == -1) {
+            perror("lstat broke");
+            printf("current from_ch = %s\n", from_ch);
+            lstat_broken = true;
+        }
+        if (lstat_broken) strcat(output, "SIZE_ERROR");
+        else sprintf(output + strlen(output), "%ld", st.st_size);
+        strcat(output, "): ");
+        write(archive_desc, &lvl, sizeof(lvl));
+        write(archive_desc, "f", 1);
+        write(archive_desc, from_ch, strlen(from_ch));
+        int src_desc, dst_desc, in, out;
+        if ((src_desc = open(from_ch, O_RDONLY)) == -1) {
+            char buf[PATH_MAX + 29] = "Could not open source file ";
+            strcat(buf, from_ch);
+            strcat(buf, "\n");
+            perror(buf);
+            failure = true;
         }
         else {
-            //copy file
-            strcat(output, "File ");
-            strcat(output, obj->d_name);
-            strcat(output, " (");
-            //mb %d
-            sprintf(output + strlen(output), "%ld", st.st_size);
-            strcat(output, "): ");
-            if (file_pack(from_ch, lvl + 1) == 0) {
-                strcat(output, "SUCCESS\n");
-            } else {
-                strcat(output, "FAIL\n");
+            char buf[BUFSIZ];
+            ssize_t read_status, write_status;
+            while ((read_status = read(src_desc, buf, BUFSIZ)) > 0) {
+                if ((write_status = write(archive_desc, buf, BUFSIZ)) == -1) {
+                    failure = true;
+                    perror("Copying error occured");
+                    break;
+                }
             }
+            if (read_status == -1) {
+                failure = true;
+                perror("Copying error occured");
+            }
+            close(src_desc);
         }
+        if (failure) strcat(output, "FAIL\n");
+        else strcat(output, "SUCCESS\n");
         write(log_desc, output, strlen(output));
-        CutAdress(from_ch);
     }
-    from_ch[strlen(from_ch)-1] = '\0';
-    CutAdress(from_ch);
-    //chdir("..");
-    closedir(origin);
+    if (failure) return -1;
     return 0;
 }
 
@@ -141,54 +195,37 @@ int main(int argc, char* argv[]){
         perror("Current working directory is unavailable");
         return -1;
     }
+    size_t lenpath = strlen(path);
     if (argc < 3) {
         perror("Not enough arguments");
         return -1;
     }
-    if (strcmp(argv[1], "-pack") == 0) {
-        int i;
-        char bruh[PATH_MAX];
-        strcpy(bruh, path);
-        log_desc = open(strcat(bruh, "/log.txt"), O_RDWR | O_APPEND | O_CREAT, S_IRWXO | S_IRWXG | S_IRWXU);
-        archive_desc = open(argv[2], O_RDWR | O_APPEND | O_CREAT, S_IRWXO | S_IRWXG | S_IRWXU);
-        if (log_desc == -1) {
-            perror("log.txt doesn't open ");
-            return -1;
-        }
-        if (archive_desc == -1) {
-            perror("Could not create archive file");
-            return -1;
-        }
-        //read - write || write to end || create it if needed, write/read/execute
-        // date + ls
-        time_t t = time(NULL);
-        struct tm d = *localtime(&t);
-        char buf[400];
-        write(log_desc, "Hour and date : ", 17);
-        //putenv("TZ=Europe/Moscow");
-        //sprintf(buf, asctime(localtime(&t)));
-        strftime(buf, 400, "%X %d %B %Y\n", &d);
-        write(log_desc, buf, strlen(buf));
+    char buf[PATH_MAX];
+    log_desc = open("log.txt", O_RDWR | O_APPEND | O_CREAT, S_IRWXU | S_IRGRP);
+    archive_desc = open(argv[2], O_RDWR | O_APPEND | O_CREAT, S_IRWXO | S_IRWXG | S_IRWXU);
+    if (log_desc == -1) {
+        perror("log.txt doesn't open ");
+        return -1;
+    }
+    if (archive_desc == -1) {
+        perror("Could not create archive file");
+        return -1;
+    }
+    //read - write || write to end || create it if needed, write/read/execute
+    // date + ls
+    time_t t = time(NULL);
+    struct tm d = *localtime(&t);
+    write(log_desc, "Hour and date : ", 17);
+    strftime(buf, 400, "%X %d %B %Y\n", &d);
+    write(log_desc, buf, strlen(buf));
+    if (strcmp(argv[1], "-p") == 0) {
         chdir("..");
-        if (strcmp(argv[3], "-d") == 0) { // archive whole directory
-            strcat(path, "/");
-            strcat(path, argv[4]);
-            //inputting full adress of input
-            dir_pack(path, 0);
-        }
-        else if (strcmp(argv[3], "-f") == 0) {
-            //list of files
-        }
-        else {
-            perror("Wrong parameter for input");
-            return -1;
-        }
-        if (close(log_desc) == -1) {
-            perror("log.txt doesn't close ");
-            return -1;
+        for (int i = 3; i < argc; ++i) {
+            sprintf(path + lenpath, "/%s", argv[i]);
+            pack(path, 0);
         }
     }
-    else if (strcmp(argv[1], "-unpack") == 0) {
+    else if (strcmp(argv[1], "-u") == 0) {
         //
     }
     else {
@@ -197,6 +234,10 @@ int main(int argc, char* argv[]){
     }
     if (close(archive_desc) == -1) {
         perror("Archive file didn't close");
+        return -1;
+    }
+    if (close(log_desc) == -1) {
+        perror("log.txt doesn't close ");
         return -1;
     }
     return 0;
